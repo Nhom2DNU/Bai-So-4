73d842aed85e13994dc7b7b6d39d78f99d253393
pangolin-hmt <laodai1691@gmail.com>
2015-05-26 22:15:39 +0700

    Tran Tien Dat

---
+++ bai29.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+void nhap(int a[], int &n);
+void main(){
+	int x, n;
+	int a[100];
+	cout << "Nhap x = "; cin >> x; cin.ignore();
+	nhap(a, n);
+	for (int i = 1; i <= n; i++){
+		if (a[i] < x)cout << "\na[" << i << "] = " << a[i] << "    Nho hon x \n"; 
+	}
+	cin.get();
+}
+void nhap(int a[], int &n)
+{
+	cout << "Nhap n = "; cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		cout << "a[" << i << "] = "; cin >> a[i]; cin.ignore();
+	}
+
+}
